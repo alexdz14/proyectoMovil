7ffc86fd701711c09dae0294d17ced99e20c1ca5
alexdz14 <zs22016132@estudiantes.uv.mx>
2025-05-25 21:22:52 -0600

    addproducto

--- MyApplication/app/src/main/java/com/example/myapplication/Perfil.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Perfil.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class addProducto : AppCompatActivity() {
+class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_producto)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layoutaddProducto)) { v, insets ->
+        setContentView(R.layout.activity_perfil)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
