3be394fc7478d6367ace4d82196125949ba8fdcb
angel.alducin <zs22016132@estudiantes.uv.mx>
2025-05-25 19:45:09 -0600

    Modificaciones

--- MyApplication/app/src/main/java/com/example/myapplication/addProducto.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/addProducto.kt
@@ -1,32 +1,16 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
-import android.widget.TextView // Importa la clase TextView
 
-class MainActivity : AppCompatActivity() {
+class addProducto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val btnVendedor = findViewById<Button>(R.id.btnVendedor)
-        btnVendedor.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-        }
-
-        val textRegistro = findViewById<TextView>(R.id.textRegistro)
-        textRegistro.setOnClickListener {
-            val intent = Intent(this, addProducto::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_add_producto)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
